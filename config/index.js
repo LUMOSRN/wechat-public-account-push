cd9a0f71f175e1cf77d3f880921482c41d91a119
LUMOSRN <97089152+LUMOSRN@users.noreply.github.com>
2022-09-13 09:07:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o3kuC6syZJqbbsokfQpQ8JKRP0Kg",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "jhsUjKvM1oKreVQ5BpnmNh3mXQuNiklt7ZhftwYW0Xc",
+      useTemplateId: "KsW-aJQBtMc2hERhv_RyVNd6RJdt4uQxYZqO8s_DBFA",
       // 所在省份
       province: "河北",
       // 所在城市
